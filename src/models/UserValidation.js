456d5cfae5819fb901ef693ba828b91419d919b2
Michael Mena <michael.mena2408@gmail.com>
2023-09-13 01:18:33 -0300

    update user validation

--- src/models/UserValidation.js
+++ src/models/UserValidation.js
@@ -4,37 +4,46 @@ import JoiDate from '@joi/date'
 const JoiWithDate = Joi.extend(JoiDate)
 
 const userLoginSchema = Joi.object().keys({
-  email: Joi.string().email().min(6).max(20).required().messages({
+  email: Joi.string().email().min(4).max(50).required().messages({
     'string.email': 'Email must be a valid email',
     'string.min': 'Email must be at least 4 characters long',
     'string.max': 'Email must be at most 20 characters long',
     'string.empty': 'Email is a required field',
     'any.required': 'Email is a required field',
   }),
-  password: Joi.string().alphanum().min(6).max(20).required().messages({
-    'string.alphanum': 'Password must be alphanumeric',
-    'string.min': 'Password must be at least 6 characters long',
-    'string.max': 'Password must be at most 20 characters long',
-    'string.empty': 'Password is a required field',
-    'any.required': 'Password is a required field',
-  }),
+  password: Joi.string()
+    .pattern(new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$'))
+    .min(6)
+    .max(20)
+    .required()
+    .messages({
+      'string.pattern.base':
+        'Password must contain at least one numeric character, one lowercase letter, and one uppercase letter',
+      'string.min': 'Password must be at least 6 characters long',
+      'string.max': 'Password must be at most 20 characters long',
+      'string.empty': 'Password is a required field',
+      'any.required': 'Password is a required field',
+    }),
 })
 
 const userDetailSchema = userLoginSchema.keys({
   // Incluye las validaciones de userLoginSchema
-  name: Joi.string().required().messages({
+  name: Joi.string().max(20).required().messages({
+    'string.max': 'Name must be at most 20 characters long',
     'string.empty': 'Name is a required field',
     'any.required': 'Name is a required field',
   }),
-  surname: Joi.string().required().messages({
+  surname: Joi.string().max(20).required().messages({
+    'string.max': 'Surname must be at most 20 characters long',
     'string.empty': 'Surname is a required field',
     'any.required': 'Surname is a required field',
   }),
-  country: Joi.string().required().messages({
+  country: Joi.string().max(20).default(null).messages({
+    'string.max': 'Country must be at most 20 characters long',
     'string.empty': 'Country is a required field',
     'any.required': 'Country is a required field',
   }),
-  birthDate: JoiWithDate.date().format('YYYY-MM-DD').required().messages({
+  birthDate: JoiWithDate.date().format('YYYY-MM-DD').default(null).messages({
     'date.format': 'Birth date must be in format YYYY-MM-DD',
     'date.base': 'Birth date must be a valid date',
     'any.required': 'Birth date is a required field',
